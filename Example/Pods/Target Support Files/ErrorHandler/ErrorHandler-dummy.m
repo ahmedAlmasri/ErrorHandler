1391f0e298e565a907379fb436807fe6b4ab08e0
Ahmad Almasri <aalmasri@winfooz.com>
2020-09-03 12:00:46 +0300

    [EH-0]-[DONE]- ignore pod files

--- Example/Pods/Target Support Files/ErrorHandler/ErrorHandler-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ErrorHandler : NSObject
-@end
-@implementation PodsDummy_ErrorHandler
-@end
--- Example/Pods/Target Support Files/Pods-ErrorHandler_Tests/Pods-ErrorHandler_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ErrorHandler_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ErrorHandler_Tests
-@end
